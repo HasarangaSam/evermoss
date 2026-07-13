9d6b5ee6598dfad1b5b57f5e716b9381d708a9bc
Hasaranga <hasarangasamarakoon@gmail.com>
2026-07-13 10:09:33 +0530

    Fixed Loading

--- app/products/[slug]/loading.js
+++ app/products/[slug]/loading.js
@@ -2,10 +2,12 @@ import "./loading.css";
 
 export default function Loading() {
   return (
-    <div className="skeleton-container">
-      <div className="skeleton-card"></div>
-      <div className="skeleton-card"></div>
-      <div className="skeleton-card"></div>
+    <div className="loading-container">
+      <div className="loader">
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
     </div>
   );
 }
